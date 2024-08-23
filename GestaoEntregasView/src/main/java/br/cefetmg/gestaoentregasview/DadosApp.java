086db992a08f0b5190c44efa8e4be273e36d789a
exoticlucas <lulucas2007@hotmail.com>
2024-08-23 17:16:04 -0300

    Adicionando dependencias de volta

--- GestaoEntregasView/src/main/java/br/cefetmg/gestaoentregasview/DadosApp.java
+++ GestaoEntregasView/src/main/java/br/cefetmg/gestaoentregasview/DadosApp.java
@@ -23,7 +23,7 @@ public class DadosApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 //scene = new Scene(loadFXML("primary"), 640, 480);
-        scene = new Scene(loadFXML("FXMLLoginController"), 640, 480);
+        scene = new Scene(loadFXML("CadastroAtendente"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
